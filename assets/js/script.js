5643d9d35e1a99ec1700e5ab1382d45031075e43
Mateo Peñaranda <mateo_1999_7@outlook.es>
2022-06-01 07:33:39 -0500

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+
+// Referencias HTML
+
+const body = document.querySelector('#hiddenBody');
+const onLoad = document.querySelector('#onLoad');
+
+
+window.onload = () =>{
+
+    // Probar
+    // setTimeout(() => {
+    //     onLoad.style.display = 'none';
+    //     body.style.overflow = 'visible';
+    // },3000)
+
+    onLoad.style.display = 'none';
+    body.style.overflow = 'visible';
+}
+
+
